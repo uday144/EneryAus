f843aee5a916d40d1d1700ea312ba3a21543d5c1
Uday Singh <uday144@gmail.com>
2019-07-05 13:09:40 +0530

    Sorted result

--- app/src/main/java/com/example/musicfestival/MainActivity.java
+++ app/src/main/java/com/example/musicfestival/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.musicfestival;
 
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -81,24 +84,48 @@ public class MainActivity extends AppCompatActivity {
                          RecordLabelTv.setText("Record Label: "+bands[i].get(j).getRecordLabel());
                          container.addView(bandTv);
                          container.addView(RecordLabelTv);
-                         festBandsList.add(new FestBand(bands[i].get(j).getName(), bands[i].get(j).getRecordLabel(), musicFestivalName[i]));
-
+                         String rcLabel = bands[i].get(j).getRecordLabel() == null?"":bands[i].get(j).getRecordLabel();
+                         festBandsList.add(new FestBand(bands[i].get(j).getName(), rcLabel, musicFestivalName[i]));
                      }
-                }
-                Map<String,List<FestBand>> FestBandByrecordLabel = new HashMap<>();
-
-                for(FestBand p : festBandsList){
-                    if(!FestBandByrecordLabel.containsKey(p.getRecordLabel())){
-                        FestBandByrecordLabel.put(p.getRecordLabel(), new ArrayList<FestBand>());
                     }
-                    FestBandByrecordLabel.get(p.getRecordLabel()).add(p);
-                }
+                final TextView title = new TextView(MainActivity.this);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setTextSize(TypedValue.COMPLEX_UNIT_SP,30);
+                title.setPadding(20, 0, 0, 20);
+                title.setText("=======Result=======");
 
-                FestBandByrecordLabel =  festBandsList.stream()
-                        .collect(Collectors.groupingBy(FestBand::getRecordLabel));
-                System.out.println("Data grouped by Record label: "
-                        + FestBandByrecordLabel);
+                Collections.sort(festBandsList, new Comparator<FestBand>() {
+                    @Override
+                    public int compare(FestBand lhs, FestBand rhs) {
+                        return lhs.getRecordLabel().compareTo(rhs.getRecordLabel());
+                    }
+                });
+                String temp = "temprecord";
+                container.addView(title);
+                for (int i = 0; i < festBandsList.size(); i++){
+                    if(!temp.equals(festBandsList.get(i).getRecordLabel())) {
+                        temp = festBandsList.get(i).getRecordLabel();
+                        final TextView tv = new TextView(MainActivity.this);
+                        tv.setTypeface(null, Typeface.BOLD);
+                        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+                        tv.setPadding(10, 0, 0, 10);
+                        // set some properties of rowTextView or something
+                        tv.setText("Record Label: " + festBandsList.get(i).getRecordLabel());
+                        container.addView(tv);
+                    }
+                    final TextView tv2 = new TextView(MainActivity.this);
+                    final TextView tv3 = new TextView(MainActivity.this);
+                    tv2.setPadding(20, 5, 0, 5);
+                    tv3.setPadding(40, 5, 0, 5);
+                    // set some properties of rowTextView or something
+                    tv2.setText("Band Name: "+festBandsList.get(i).getBandName());
+                    tv3.setText("Festival Name: "+festBandsList.get(i).getFestName());
+
+                    container.addView(tv2);
+                    container.addView(tv3);
+                    }
             }
+
 
             @Override
             public void onFailure(Call<List<MusicFestival>> call, Throwable t) {
